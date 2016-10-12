1b114446820ba8e816e50461d12c00e0bb2d14f4
Alexander Jaeger <alexander.jaeger@me.com>
2016-10-12 23:06:01 +0200

    added requirements via setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(name='gooh',
       version='1.0.0',
       description='Language agnostic semantic versioner using git tags',
       url='http://github.com/lxhunter/gooh',
-      author='Alexander Jäger',
+      author='Alexander Jaeger',
       author_email='alexander.jaeger@me.com',
       license='MIT',
       scripts=['bin/gooh'],
       packages=['gooh'],
-      zip_safe=False
+      zip_safe=False,
+      install_requires=[
+          'semantic_version', 'sh', 'pyyaml']
       )
