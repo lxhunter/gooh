56378767f8a638832880a0d0c0c631694728eaaf
Alexander Jaeger <alexander.jaeger@me.com>
2016-10-13 00:10:43 +0200

    testing release

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='gooh',
-      version='1.0.3',
+      version='1.0.4',
       description='Language agnostic semantic versioner using git tags',
       url='http://github.com/lxhunter/gooh',
-      download_url = 'http://github.com/lxhunter/gooh/tarball/1.0.3',
+      download_url = 'http://github.com/lxhunter/gooh/tarball/1.0.4',
       author='Alexander Jaeger',
       author_email='alexander.jaeger@me.com',
       license='MIT',
