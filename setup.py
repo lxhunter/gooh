1c4ab2d2445b9c1ef845894cfb459bf4d5533dd9
Alexander Jäger <alexander.jaeger@me.com>
2017-08-18 14:45:30 +0200

    added error message and default action

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='gooh',
-      version='1.0.5',
+      version='1.0.6',
       description='Language agnostic semantic versioner using git tags',
       url='http://github.com/lxhunter/gooh',
-      download_url = 'http://github.com/lxhunter/gooh/tarball/1.0.5',
+      download_url = 'http://github.com/lxhunter/gooh/tarball/1.0.6',
       author='Alexander Jaeger',
       author_email='alexander.jaeger@me.com',
       license='MIT',
